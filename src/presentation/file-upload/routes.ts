535c1089e8a607fbe6c9620d0ac60cb080ac31be
Francisco Soto <panchon.ues@gmail.com>
2024-06-18 23:44:32 -0700

    subdir and middleware

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 import { FileUploadController } from './controller';
 import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
 
 
 
@@ -15,7 +16,10 @@ export class FileUploadRoutes {
     
     const fileUploadService = new FileUploadService();
     const controller = new FileUploadController(fileUploadService);
-    
+
+
+    //Middlewares aplicados a ambos enpoints
+    router.use(FileUploadMiddleware.containFiles);
     // Definir las rutas
     router.post('/single/:type', controller.uploadFile);
     router.post('/multiple/:type', controller.uploadMultipleFiles);
